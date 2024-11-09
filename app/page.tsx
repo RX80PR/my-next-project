d159fa5cdbe606f2b3ba685d589a6529702cce4f
RX80PR <ktc24a31c0006@edu.kyoto-tech.ac.jp>
2024-11-09 16:09:42 +0900

    46ページ

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Content, Public_Sans } from "next/font/google";
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import ButtonLink from "@/app/_components/ButtonLink";
+
 type News = {
   id: string;
   title: string;
@@ -22,8 +24,8 @@ const data: {
       category: {
         name: "更新情報",
       },
-      publishedAt: "2023-05-19",
-      createdAt: "2023-05-19",
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
     },
     {
       id: "3",
@@ -31,15 +33,27 @@ const data: {
       category: {
         name: "更新情報",
       },
-      publishedAt: "2023-05-19",
-      createdAt: "2023-05-19",
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
     },
-  ]
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+   ],
 };
 
 
 export default function Home() {
+  const sliceData = data.contents.slice(0, 2);
+
   return (
+    <>
   <section className={styles.top}>
     <div>
       <h1 className={styles.title}>テクノロジーで世界を変える</h1>
@@ -55,5 +69,42 @@ export default function Home() {
       height={1200}
     />
   </section>
+  <section className={styles.news}>
+    <h2 className={styles.newstitle}>News</h2>
+    <ul>
+      {sliceData.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="No Image"
+              width={1200}
+              height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.newsItemTitle}>
+{article.title}</dt>
+              <dd className={styles.meta}>
+                <span className={styles.tag}>
+{article.category.name}</span>
+                    <span className={styles.date}>
+                      <Image
+                        src="/clock.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        priority
+                      />
+                      {article.publishedAt}
+                    </span>
+                 </dd>
+               </dl>
+            </div>
+          </li>
+      ))}
+        </ul>
+      </section>
+    </>
   );
 }
